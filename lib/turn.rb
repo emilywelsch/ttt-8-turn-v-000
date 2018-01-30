bb9f219209de0ec2153e73c13ec06694f501141a
Emily Welsch <emwelsch@gmail.com>
2018-01-30 18:05:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,4 @@
+# define #display_board
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -6,4 +7,20 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-print display_board
+
+board = []
+index = 0
+def valid_move?(board, index)
+  if index.between?(0,8) == true && position_taken?(board,index) == false
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+  board[index] != ""
+  board[index] == "nil"
+  board[index] == "X" || board[index] == "O"
+end
